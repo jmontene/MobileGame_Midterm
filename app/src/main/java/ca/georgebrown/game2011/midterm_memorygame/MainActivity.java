60fe9b29791f0cffe116325ca7ba8bd133465465
JamieCCH <jamie.cch@gmail.com>
2018-03-12 09:29:14 -0400

    PART 1 Done

--- app/src/main/java/ca/georgebrown/game2011/midterm_memorygame/MainActivity.java
+++ app/src/main/java/ca/georgebrown/game2011/midterm_memorygame/MainActivity.java
@@ -3,7 +3,11 @@ package ca.georgebrown.game2011.midterm_memorygame;
 import android.app.Activity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends Activity {
+
+    ArrayList<card>
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
